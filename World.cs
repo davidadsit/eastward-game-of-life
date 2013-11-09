7f2e8e3e5b17ca590b4177a3864885e57325e089
david-adsit <david-adsit@pluralsight.com>
2013-11-09 08:50:18 -0700

    It works.

--- World.cs
+++ World.cs
@@ -41,9 +41,55 @@ namespace GameOfLife
         public void Evolve()
         {
             generation++;
-            if (livingCells.Count < 3)
+            var newWorld = new List<Tuple<int, int>>();
+            AddCellsThatWillContinueToLive(newWorld);
+            AddCellsThatWillComeToLife(newWorld);
+            livingCells = newWorld;
+        }
+
+        void AddCellsThatWillComeToLife(List<Tuple<int, int>> newWorld)
+        {
+            if (!livingCells.Any())
             {
-                livingCells = new List<Tuple<int, int>>();
+                return;
+            }
+            var rowNumbers = livingCells.Select(x => x.Item1);
+            var columnNumbers = livingCells.Select(x => x.Item2);
+            for (var row = rowNumbers.Min() - 1; row <= rowNumbers.Max() + 1; row++)
+            {
+                for (var column = columnNumbers.Min() - 1; column <= columnNumbers.Max() + 1; column++)
+                {
+                    if (TheCellIsDeadAndShouldLive(row, column))
+                    {
+                        newWorld.Add(new Tuple<int, int>(row, column));
+                    }
+                }
+            }
+        }
+
+        bool TheCellIsDeadAndShouldLive(int row, int column)
+        {
+            if (livingCells.Any(x => x.Item1 == row && x.Item2 == column)) return false;
+            var livingNeighbors = livingCells.Count(x => x.Item1 >= (row - 1) &&
+                                                         x.Item1 <= (row + 1) &&
+                                                         x.Item2 >= (column - 1) &&
+                                                         x.Item2 <= (column + 1));
+            return livingNeighbors == 3;
+        }
+
+        void AddCellsThatWillContinueToLive(List<Tuple<int, int>> newWorld)
+        {
+            foreach (var livingCell in livingCells)
+            {
+                var livingNeighbors =
+                    livingCells.Count(x => x.Item1 >= (livingCell.Item1 - 1) &&
+                                           x.Item1 <= (livingCell.Item1 + 1) &&
+                                           x.Item2 >= (livingCell.Item2 - 1) &&
+                                           x.Item2 <= (livingCell.Item2 + 1)) - 1;
+                if (livingNeighbors == 2 || livingNeighbors == 3)
+                {
+                    newWorld.Add(livingCell);
+                }
             }
         }
     }
--- WorldSpecs.cs
+++ WorldSpecs.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using System.Text;
+using Moq;
 using NUnit.Framework;
 
 namespace GameOfLife
@@ -15,6 +16,72 @@ namespace GameOfLife
 
         World World;
         Mock<IPrinter> Printer;
+
+        [Test]
+        public void When_a_world_with_a_cell_with_no_living_neighbors_evolves()
+        {
+            World.AddLivingCell(0, 0);
+
+            World.Evolve();
+            World.Print(Printer.Object);
+
+            Printer.Verify(x => x.PrintRow("Generation: 1"));
+            Printer.Verify(x => x.PrintRow("-"));
+        }
+
+        [Test]
+        public void When_an_empty_world_evolves()
+        {
+            World.Evolve();
+            World.Print(Printer.Object);
+
+            Printer.Verify(x => x.PrintRow("Generation: 1"));
+            Printer.Verify(x => x.PrintRow("-"));
+        }
+
+        [Test]
+        public void When_a_world_with_a_cell_with_one_living_neighbors_evolves()
+        {
+            World.AddLivingCell(0, 0);
+            World.AddLivingCell(0, 1);
+
+            World.Evolve();
+            World.Print(Printer.Object);
+
+            Printer.Verify(x => x.PrintRow("Generation: 1"));
+            Printer.Verify(x => x.PrintRow("-"));
+        }
+
+        [Test]
+        public void When_a_world_with_a_static_block_of_four_living_neighbors_evolves()
+        {
+            World.AddLivingCell(0, 0);
+            World.AddLivingCell(0, 1);
+            World.AddLivingCell(1, 0);
+            World.AddLivingCell(1, 1);
+
+            World.Evolve();
+            World.Print(Printer.Object);
+
+            Printer.Verify(x => x.PrintRow("Generation: 1"));
+            Printer.Verify(x => x.PrintRow("----"), Times.Exactly(2));
+            Printer.Verify(x => x.PrintRow("-OO-"), Times.Exactly(2));
+        }
+
+        [Test]
+        public void When_a_world_with_an_oscillating_block_of_living_neighbors_evolves()
+        {
+            World.AddLivingCell(0, 0);
+            World.AddLivingCell(0, 1);
+            World.AddLivingCell(0, 2);
+
+            World.Evolve();
+            World.Print(Printer.Object);
+
+            Printer.Verify(x => x.PrintRow("Generation: 1"));
+            Printer.Verify(x => x.PrintRow("---"), Times.Exactly(2));
+            Printer.Verify(x => x.PrintRow("-O-"), Times.Exactly(3));
+        }
 
         [Test]
         public void When_an_new_world_is_print()
@@ -46,47 +113,6 @@ namespace GameOfLife
             Printer.Verify(x => x.PrintRow("Generation: 0"));
             Printer.Verify(x => x.PrintRow("----"), Times.Exactly(2));
             Printer.Verify(x => x.PrintRow("-OO-"), Times.Once);
-        }
-
-        [Test]
-        public void When_a_world_with_a_cell_with_no_living_neighbors_evolves()
-        {
-            World.AddLivingCell(0,0);
-
-            World.Evolve();
-            World.Print(Printer.Object);
-
-            Printer.Verify(x => x.PrintRow("Generation: 1"));
-            Printer.Verify(x => x.PrintRow("-"));
-        }
-
-        [Test]
-        public void When_a_world_with_a_cell_with_one_living_neighbors_evolves()
-        {
-            World.AddLivingCell(0,0);
-            World.AddLivingCell(0,1);
-
-            World.Evolve();
-            World.Print(Printer.Object);
-
-            Printer.Verify(x => x.PrintRow("Generation: 1"));
-            Printer.Verify(x => x.PrintRow("-"));
-        }
-
-        [Test]
-        public void When_a_world_with_a_cell_with_two_living_neighbors_evolves()
-        {
-            World.AddLivingCell(0,0);
-            World.AddLivingCell(0,1);
-            World.AddLivingCell(1,0);
-            World.AddLivingCell(1,1);
-
-            World.Evolve();
-            World.Print(Printer.Object);
-
-            Printer.Verify(x => x.PrintRow("Generation: 1"));
-            Printer.Verify(x => x.PrintRow("----"), Times.Exactly(2));
-            Printer.Verify(x => x.PrintRow("-OO-"), Times.Exactly(2));
         }
     }
 }
